82a106e8f7aab01e87b5732a9b8fabdb0868c4b1
isaacziroldo <50035007+isaacziroldo@users.noreply.github.com>
2022-06-08 09:41:28 -0300

    Initial commit

---
+++ src/components/Post.jsx
@@ -0,0 +1,42 @@
+
+import styles from './Post.module.css'
+
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/50035007?v=4" alt="" />
+                    <div className={styles.authorInfo}>
+                        <strong>Isaac Zidoldo</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+
+                <time title='07 de junho às 20h54' dateTime='2022-05-11'>Publicado há 1h</time>
+
+                
+
+            </header>
+
+            <div className={styles.content}>
+                    
+                    <p>Fala galeraa 👋</p>
+
+                    <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                    <p> <a href=""> 👉  jane.design/doctorcare</a></p>
+
+                    <p> 
+                    <a href=""> #novoprojeto </a>{' '}
+                    <a href=""> #nlw </a>{' '}
+                    <a href="">  #rocketseat</a>{' '}
+                    </p>
+                    
+                </div>
+
+        </article>
+    )
+}
